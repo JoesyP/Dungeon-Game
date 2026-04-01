d43013422d1e72b0ba1af2942b6337429d8b406c
JoesyP <157426260+JoesyP@users.noreply.github.com>
2026-04-01 15:08:10 -0500

    polishing

--- DungeonCleanup/Assets/Scripts/GameManager.cs
+++ DungeonCleanup/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +13,10 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI tutorialText;
     public TextMeshProUGUI coinText;
 
+    public GameObject coinPrefab;
+    public Transform[] coinSpawnPoints;
+    public int coinsToSpawn = 3;
+
     void Awake()
     {
         instance = this;
@@ -19,10 +24,34 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        SpawnCoins(); 
         UpdateUI();
 
         if (exitPortal != null)
             exitPortal.SetActive(false);
+    }
+
+    void SpawnCoins()
+    {
+        List<Transform> availablePoints = new List<Transform>(coinSpawnPoints);
+
+        // Shuffle spawn points
+        for (int i = 0; i < availablePoints.Count; i++)
+        {
+            Transform temp = availablePoints[i];
+            int randomIndex = Random.Range(i, availablePoints.Count);
+
+            availablePoints[i] = availablePoints[randomIndex];
+            availablePoints[randomIndex] = temp;
+        }
+
+        // Spawn coins at unique positions
+        for (int i = 0; i < coinsToSpawn && i < availablePoints.Count; i++)
+        {
+            Instantiate(coinPrefab, availablePoints[i].position, Quaternion.identity);
+        }
+
+        coinsRequired = coinsToSpawn; // keeps UI correct
     }
 
     public void CollectCoin()
